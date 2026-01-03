c48a56472ef2ce5b895b4559b330faf75e846171
stephanie <stephaniesomadee@gmail.com>
2026-01-03 13:54:13 +0000

    initial commit: adding user context and page

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,16 +1,36 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export const UserContext = createContext();
+
+const initialUsers = [
+  { id: 1, name: "Alice", role: "admin", active: true },
+  { id: 2, name: "Bob", role: "viewer", active: true },
+  { id: 3, name: "Trina", role: "admin", active: true },
+  { id: 4, name: "Sam", role: "viewer", active: false },
+  { id: 5, name: "Billy", role: "editor", active: false },
+  { id: 6, name: "Robert", role: "support", active: true },
+  { id: 7, name: "Samantha", role: "viewer", active: false },
+]
 export const UserProvider = ({ children }) => {
-  const [users, setUsers] = useState([
-    { id: 1, name: "Alice", role: "admin", active: true },
-    { id: 2, name: "Bob", role: "viewer", active: true },
-    { id: 2, name: "Trina", role: "admin", active: true },
-    { id: 2, name: "Sam", role: "viewer", active: true },
-  ]);
+  const [users, setUsers] = useState(initialUsers);
+
+  const addUser = (name, role, active) => { 
+    const newUser = {
+      id: uuidv4(),
+      name,
+      role,
+      active
+    };
+    setUsers(prev => [...prev, newUser])
+  }
+
+  const removeUser = (id) => { 
+    setUsers(prev => prev.filter(user => user.id !== id));
+  }
 
   return (
-    <UserContext.Provider value={{ users, setUsers }}>
+    <UserContext.Provider value={{ users, setUsers, addUser, removeUser}}>
       {children}
     </UserContext.Provider>
   );
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,26 +1,50 @@
 import { useContext } from "react";
-import { UserContext } from "../context/UserContext"; // named import
-
+import { UserContext } from "../context/UserContext";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 export default function UsersPage() {
-  const { users } = useContext(UserContext);
+  const { users, removeUser } = useContext(UserContext)
+  // addUser} = useContext(UserContext);
 
   return (
     <div>
       <h1>Users Page</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name} ({user.role})</li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table aria-label="user table">
+
+          <TableHead>
+            <TableRow>
+              <TableCell scope="col">iD</TableCell>
+              <TableCell scope="col">Name</TableCell>
+              <TableCell scope="col">User Acesss</TableCell>
+              <TableCell scope="col">Active</TableCell>
+              <TableCell scope="col">Remove User?</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.name}
+                sx={{}}>
+                <TableCell scope="row" component="th">
+                  {user.id}
+                </TableCell>
+                <TableCell scope="row" component="th">
+                  {user.name}
+                </TableCell>
+                <TableCell scope="row">{user.role}</TableCell>
+                <TableCell scope="row">{user.active === true ? 'Yes' : 'No'}</TableCell>
+                {user.role !== "admin" ? <TableCell><button onClick={() => removeUser(user.id)}>Remove User</button></TableCell> : <TableCell> <h5> Cannot remove user</h5></TableCell>}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-}
-
-
-// <ul>
-//         {users.map(user => (
-//           <li key={user.id}>
-//             {user.name} ({user.role})
-//           </li>
-//         ))}
-//       </ul>
+};
